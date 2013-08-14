d4d7008790cd9d8a34bef20437a689ff07bf0e29
Mark Hambly <hambly@gmail.com>
2013-08-15 10:51:35 +1200

    Initial Commit

---
+++ Rugby/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Rugby
+//
+//  Created by Mark on 8/15/13.
+//  Copyright (c) 2013 Mark Hambly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Rugby/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Rugby
+//
+//  Created by Mark on 8/15/13.
+//  Copyright (c) 2013 Mark Hambly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
